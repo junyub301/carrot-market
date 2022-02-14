5f13715c4a6dc2bbeeea654d8f60c336a745194d
junyub301 <junyub0301@gmail.com>
2022-02-14 20:45:01 +0900

    Auth part 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
         extend: {},
     },
     darkMode: "media", //media: 자신의 컴퓨터 환경설정에 따라감 class: 토글 버튼을 통해 다크모드 설정
-    plugins: [],
+    plugins: [require("@tailwindcss/forms")],
 };
