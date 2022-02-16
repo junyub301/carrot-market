59cfe68893e484e10334ae8f639339d20a6845cd
junyub301 <junyub0301@gmail.com>
2022-02-16 20:37:00 +0900

    Upload Item

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -42,13 +42,17 @@ export default function Enter() {
                     </div>
                 </div>
                 <form className='flex flex-col mt-8'>
-                    <label className='text-sm font-medium text-gray-700'>
+                    <label
+                        htmlFor='input'
+                        className='text-sm font-medium text-gray-700'
+                    >
                         {method === "email" ? "Email address" : null}
                         {method === "phone" ? "Phone number" : null}
                     </label>
                     <div className='mt-1'>
                         {method === "email" ? (
                             <input
+                                id='input'
                                 type='email'
                                 className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500'
                                 required
@@ -60,6 +64,7 @@ export default function Enter() {
                                     +82
                                 </span>
                                 <input
+                                    id='input'
                                     className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500'
                                     type='number'
                                     required
