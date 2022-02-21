8d74d210d41abcc1f8deb7c6cb8e10ea48593025
junyub301 <junyub0301@gmail.com>
2022-02-21 20:53:57 +0900

    Api Routes

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-
-client.user.create({
-    data: {
-        email: "test2@test.com",
-        name: "test2",
-    },
-});
+export default new PrismaClient();
