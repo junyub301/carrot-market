edb1888351afd8b3f82b2120aa76926124262fde
junyub301 <junyub0301@gmail.com>
2022-02-24 21:11:57 +0900

    Refactoring useMutation part 2

--- libs/client/useMutations.tsx
+++ libs/client/useMutations.tsx
@@ -1,15 +1,36 @@
 import { useState } from "react";
 
-export default function useMutations(
-    url: string
-): [
-    (data: any) => void,
-    { loading: boolean; data: undefined | any; error: undefined | any }
-] {
+interface UseMutationsState {
+    loading: boolean;
+    data?: object;
+    error?: object;
+}
+
+type UseMutationResult = [(data: any) => void, UseMutationsState];
+
+export default function useMutations(url: string): UseMutationResult {
+    const [state, setState] = useState({
+        loading: false,
+        data: undefined,
+        error: undefined,
+    });
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<undefined | any>(undefined);
     const [error, setError] = useState<undefined | any>(undefined);
-    function mutation(data: any) {}
+    function mutation(data: any) {
+        setLoading(true);
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json().catch(() => {}))
+            .then(setData)
+            .catch(setError)
+            .finally(() => setLoading(false));
+    }
 
     return [mutation, { loading, error, data }];
 }
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -8,5 +8,6 @@ export default async function handler(
     if (req.method !== "POST") {
         res.status(401).end();
     }
+    console.log(req.body);
     res.status(200).end();
 }
