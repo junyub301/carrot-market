c0da0d37e97167f3f3d529a03a514adf992940fe
junyub301 <junyub0301@gmail.com>
2022-03-09 21:51:12 +0900

    All Posts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -2,13 +2,31 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import FloatingButton from "@components/floating-button";
 import Layout from "@components/layout";
+import useSWR from "swr";
+import { Post, User } from ".prisma/client";
+
+interface PostWithUser extends Post {
+    user: User;
+    _count: {
+        wonderings: number;
+        answers: number;
+    };
+}
+
+interface PostsResponse {
+    ok: boolean;
+    posts: PostWithUser[];
+}
 
 const Community: NextPage = () => {
+    const { data } = useSWR<PostsResponse>(`/api/posts`);
+    console.log(data);
+
     return (
         <Layout title='동네생활' hasTabBar>
             <div className='py-16 px-4 space-y-8'>
-                {[1, 2, 3, 4, 5, 6].map((_, i) => (
-                    <Link href={`/community/${i}`} key={i}>
+                {data?.posts?.map((post) => (
+                    <Link href={`/community/${post.id}`} key={post.id}>
                         <a className='flex cursor-pointer flex-col items-start'>
                             <span className='flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-900'>
                                 동네질문
@@ -16,12 +34,12 @@ const Community: NextPage = () => {
                             <div className='mt-2 text-gray-700'>
                                 <span className='text-orange-500 font-medium'>
                                     Q.
-                                </span>{" "}
-                                What is the best mandu restaurant?
+                                </span>
+                                {post.question}
                             </div>
                             <div className='mt-5 flex items-center justify-between w-full text-gray-500 font-medium text-xs'>
-                                <span>니꼬</span>
-                                <span>18시간 전</span>
+                                <span>{post.user.name}</span>
+                                <span>{post.createdAt}</span>
                             </div>
                             <div className='flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full'>
                                 <span className='flex space-x-2 items-center text-sm'>
@@ -39,7 +57,9 @@ const Community: NextPage = () => {
                                             d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
                                         ></path>
                                     </svg>
-                                    <span>궁금해요 1</span>
+                                    <span>
+                                        궁금해요 {post._count.wonderings}
+                                    </span>
                                 </span>
                                 <span className='flex space-x-2 items-center text-sm'>
                                     <svg
@@ -56,7 +76,7 @@ const Community: NextPage = () => {
                                             d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
                                         ></path>
                                     </svg>
-                                    <span>답변 1</span>
+                                    <span>답변 {post._count.answers}</span>
                                 </span>
                             </div>
                         </a>
