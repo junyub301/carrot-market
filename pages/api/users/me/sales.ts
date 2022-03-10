e3805fa96a6ebe56db667f481abb1566071364af
junyub301 <junyub0301@gmail.com>
2022-03-10 22:10:17 +0900

    Handlers

---
+++ pages/api/users/me/sales.ts
@@ -0,0 +1,35 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+) {
+    const {
+        session: { user },
+    } = req;
+
+    /* record를 사용하고 싶을경우.. 나중에 변경
+     const saless = client.record.findMany({
+        where: {
+            userId: user?.id,
+            kind: "Sale",
+        },
+    });
+    console.log(saless);
+     */
+
+    const sales = await client.sale.findMany({
+        where: {
+            userId: user?.id,
+        },
+        include: {
+            product: true,
+        },
+    });
+    res.json({ ok: true, sales });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
