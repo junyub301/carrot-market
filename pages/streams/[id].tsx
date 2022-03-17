9f1612221b5f5dfe9e91fae46ab827693c4a591a
junyub301 <junyub0301@gmail.com>
2022-03-17 20:22:44 +0900

    Mutations and Refresh

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -35,7 +35,10 @@ const StreamDetail: NextPage = () => {
     const { user } = useUser();
     const { register, handleSubmit, reset } = useForm<MessageForm>();
     const { data, mutate } = useSWR<StreamResponse>(
-        router?.query?.id ? `/api/streams/${router.query.id}` : null
+        router?.query?.id ? `/api/streams/${router.query.id}` : null,
+        {
+            refreshInterval: 1000,
+        }
     );
     const [sendMessage, { loading, data: sendMessageData }] = useMutations(
         `/api/streams/${router.query.id}/messages`
@@ -43,14 +46,28 @@ const StreamDetail: NextPage = () => {
     const onValid = (form: MessageForm) => {
         if (loading) return;
         reset();
+        mutate(
+            (prev) =>
+                prev &&
+                ({
+                    prev,
+                    stream: {
+                        ...prev.stream,
+                        messages: [
+                            ...prev.stream.messages,
+                            {
+                                id: Date.now(),
+                                message: form.message,
+                                user: { ...user },
+                            },
+                        ],
+                    },
+                } as any),
+            false
+        );
+
         sendMessage(form);
     };
-
-    useEffect(() => {
-        if (sendMessageData && sendMessageData.ok) {
-            mutate();
-        }
-    }, [sendMessageData, mutate]);
 
     return (
         <Layout canGoBack>
