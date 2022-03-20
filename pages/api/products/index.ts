01b29b6d709093ed1f3a4eeecee2d6b89ef1b09b
junyub301 <junyub0301@gmail.com>
2022-03-20 19:17:57 +0900

    Product Images

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -21,7 +21,7 @@ async function handler(
     }
     if (req.method === "POST") {
         const {
-            body: { name, price, description },
+            body: { name, price, description, photoId },
             session: { user },
         } = req;
         const product = await client.product.create({
@@ -29,7 +29,7 @@ async function handler(
                 name,
                 price: +price,
                 description,
-                image: "xx",
+                image: photoId,
                 user: {
                     connect: {
                         id: user?.id,
--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -5,7 +5,7 @@ import Layout from "@components/layout";
 import Textarea from "@components/textarea";
 import { useForm } from "react-hook-form";
 import useMutations from "@libs/client/useMutations";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Product } from ".prisma/client";
 
@@ -13,6 +13,7 @@ interface UploadProductForm {
     name: string;
     price: number;
     description: string;
+    photo: FileList;
 }
 
 interface UplaodProductMutation {
@@ -21,12 +22,24 @@ interface UplaodProductMutation {
 }
 
 const Upload: NextPage = () => {
-    const { register, handleSubmit } = useForm<UploadProductForm>();
+    const { register, handleSubmit, watch } = useForm<UploadProductForm>();
     const [uploadProduct, { loading, data }] =
         useMutations<UplaodProductMutation>("/api/products");
-    const onValid = (data: UploadProductForm) => {
+    const onValid = async ({ name, price, description }: UploadProductForm) => {
         if (loading) return;
-        uploadProduct(data);
+        if (photo && photo.length > 0) {
+            const { uploadURL } = await (await fetch(`/api/files`)).json();
+            const form = new FormData();
+            form.append("file", photo[0], name);
+            const {
+                result: { id },
+            } = await (
+                await fetch(uploadURL, { method: "POST", body: form })
+            ).json();
+            uploadProduct({ name, price, description, photoId: id });
+        } else {
+            uploadProduct({ name, price, description });
+        }
     };
     const router = useRouter();
     useEffect(() => {
@@ -34,6 +47,14 @@ const Upload: NextPage = () => {
             router.push(`/products/${data.product.id}`);
         }
     }, [data, router]);
+    const photo = watch("photo");
+    const [photoPreview, setPhotoPreview] = useState("");
+    useEffect(() => {
+        if (photo && photo.length > 0) {
+            const file = photo[0];
+            setPhotoPreview(URL.createObjectURL(file));
+        }
+    }, [photo]);
     return (
         <Layout canGoBack>
             <form
@@ -41,24 +62,36 @@ const Upload: NextPage = () => {
                 onSubmit={handleSubmit(onValid)}
             >
                 <div>
-                    <label className='w-full text-gray-600 cursor-pointer hover:text-orange-500 hover:border-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 py-6 h-48 rounded-md'>
-                        <svg
-                            className='h-12 w-12'
-                            stroke='currentColor'
-                            fill='none'
-                            viewBox='0 0 48 48'
-                            aria-hidden='true'
-                        >
-                            <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                            />
-                        </svg>
+                    {photoPreview ? (
+                        <img
+                            src={photoPreview}
+                            className='w-full text-gray-600 h-46 rounded-md'
+                        />
+                    ) : (
+                        <label className='w-full text-gray-600 cursor-pointer hover:text-orange-500 hover:border-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 py-6 h-48 rounded-md'>
+                            <svg
+                                className='h-12 w-12'
+                                stroke='currentColor'
+                                fill='none'
+                                viewBox='0 0 48 48'
+                                aria-hidden='true'
+                            >
+                                <path
+                                    d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
+                                    strokeWidth={2}
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                />
+                            </svg>
 
-                        <input className='hidden' type='file' />
-                    </label>
+                            <input
+                                {...register("photo")}
+                                accept='image/*'
+                                className='hidden'
+                                type='file'
+                            />
+                        </label>
+                    )}
                 </div>
                 <div>
                     <Input
