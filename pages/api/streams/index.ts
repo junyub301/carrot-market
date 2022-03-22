862e4a2c4b6a05f0491c42f6c919f852d0d07616
junyub301 <junyub0301@gmail.com>
2022-03-22 21:46:08 +0900

    Streaming API

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -12,8 +12,40 @@ async function handler(
         body: { name, price, description },
     } = req;
     if (req.method === "POST") {
+        const {
+            result: {
+                uid,
+                rtmps: { streamKey, url },
+            },
+        } =
+            {
+                result: {
+                    uid: "f256e6ea9341d51eea64c9454659e576",
+                    rtmps: {
+                        streamKey: "rtmps://live.cloudflare.com:443/live/",
+                        url: "MTQ0MTcjM3MjI1NDE3ODIyNTI1MjYyMjE4NTI2ODI1NDcxMzUyMzcf256e6ea9351d51eea64c9454659e576",
+                    },
+                },
+            } ||
+            (await (
+                await fetch(
+                    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/stream/live_inputs`,
+                    {
+                        method: "POST",
+                        headers: {
+                            Authorization: `Bearer ${process.env.CLOUDFLARE_STREAM_TOKEN}`,
+                        },
+                        body: `{"meta": {"name":"${name}"},"recording": { "mode": "automatic", "timeoutSeconds": 10}}`,
+                    }
+                )
+            ).json());
         const stream = await client.stream.create({
             data: {
+                cloudflareId: uid,
+                cloudflareKey: streamKey,
+                cloudflareUrl:
+                    url ||
+                    "MTQ0MTcjM3MjI1NDE3ODIyNTI1MjYyMjE4NTI2ODI1NDcxMzUyMzcf256e6ea9351d51eea64c9454659e576",
                 name,
                 price,
                 description,
