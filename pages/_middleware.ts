3a5329de6cd600eafa78e90f40866ed5142d5213
junyub301 <junyub0301@gmail.com>
2022-03-23 21:07:25 +0900

    Middlewares

---
+++ pages/_middleware.ts
@@ -0,0 +1,6 @@
+import type { NextRequest, NextFetchEvent } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    console.log("it works!");
+    console.log(req, ev);
+}
