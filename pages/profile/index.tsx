50ec90f24cde622365296bbd3f93d2fe20250da4
junyub301 <junyub0301@gmail.com>
2022-03-28 21:01:27 +0900

    SSR+Authentication

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,11 +1,12 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Link from "next/link";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { Review, User } from ".prisma/client";
 import { cls } from "@libs/client/utils";
-
+import { withSsrHandler } from "@libs/server/withSession";
+import client from "@libs/server/client";
 interface ReviewWithUser extends Review {
     createdBy: User;
 }
@@ -156,4 +157,33 @@ const Profile: NextPage = () => {
     );
 };
 
-export default Profile;
+const Page: NextPage<{ profile: User }> = ({ profile }) => {
+    return (
+        <SWRConfig
+            value={{
+                fallback: {
+                    "/api/users/me": {
+                        ok: true,
+                        profile,
+                    },
+                },
+            }}
+        >
+            <Profile />
+        </SWRConfig>
+    );
+};
+
+export const getServerSideProps = withSsrHandler(async function ({
+    req,
+}: NextPageContext) {
+    const profile = await client.user.findUnique({
+        where: { id: req?.session.user?.id },
+    });
+
+    return {
+        props: JSON.parse(JSON.stringify(profile)),
+    };
+});
+
+export default Page;
