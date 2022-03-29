22a809ec9b92a398ed3e8366a919ae2fd4c6e7a4
junyub301 <junyub0301@gmail.com>
2022-03-29 20:36:00 +0900

    getStaticProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,49 @@
 import Layout from "@components/layout";
+import { readdirSync, readFileSync } from "fs";
+import matter from "gray-matter";
+import { NextPage } from "next";
 
-export default function Blog() {
+interface Post {
+    title: string;
+    date: string;
+    category: string;
+}
+
+const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
     return (
         <Layout title='Blog' seoTitle='Blog'>
-            <h1 className='font-semibold text-lg'>Latest Posts</h1>
+            <h1 className='font-semibold text-lg text-center mb-10 mt-5'>
+                Latest Posts
+            </h1>
             <ul>
-                <li>Welcome everyone!</li>
+                {posts?.map((post: Post, index: number) => (
+                    <div key={index} className='mb-5'>
+                        <span className='text-lg text-red-500'>
+                            {post.title}
+                        </span>
+                        <div>
+                            <span>
+                                {post.date} / {post.category}
+                            </span>
+                        </div>
+                    </div>
+                ))}
             </ul>
         </Layout>
     );
+};
+
+export async function getStaticProps() {
+    const blogPosts = readdirSync("./posts").map((file) => {
+        const content = readFileSync(`./posts/${file}`, "utf-8");
+        return matter(content).data;
+    });
+
+    return {
+        props: {
+            posts: blogPosts,
+        },
+    };
 }
+
+export default Blog;
---
+++ posts/02-second-post.md
@@ -0,0 +1,9 @@
+---
+title: Second Posts
+date: 2022.02.02
+category: posts
+---
+
+# Second Post!
+
+Second
