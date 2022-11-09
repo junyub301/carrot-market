353ff84aff66ed3e3de72f4d3bc4fcc5cdd18911
junyub301 <junyub0301@gmail.com>
2022-11-09 19:58:11 +0900

    deploy test

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,14 +2,11 @@ import type { NextRequest, NextFetchEvent } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    /*  if (req.ua?.isBot ) {
+    /*  if (req.ua?.isBot as typeof req.ua) {
         return new Response("Plz don't be a bot. Be human.", { status: 403 });
     } */
     if (!req.url.includes("/api")) {
-        if (
-            !req.url.includes("/enter") &&
-            (!req.cookies["carrotsession"] as any)
-        ) {
+        if (!req.url.includes("/enter") && !req.cookies?.carrotsession) {
             return NextResponse.redirect(new URL("/enter", req.url));
         }
     }
