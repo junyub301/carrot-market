72780225c3f0087dfd959a6609d10497d2a86de5
junyub301 <junyub0301@gmail.com>
2022-11-09 18:27:51 +0900

    window fetching

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -9,7 +9,9 @@ interface ProfileResponse {
 }
 
 export default function useUser() {
-    const { data, error } = useSWR("/api/users/me");
+    const { data, error } = useSWR(
+        typeof window === "undefined" ? null : "/api/users/me"
+    );
     const router = useRouter();
     useEffect(() => {
         if (data && !data.ok) {
