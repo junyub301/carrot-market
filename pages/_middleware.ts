391e2f49fe712caf549832560b52d8c2faa726a6
junyub301 <junyub0301@gmail.com>
2022-11-09 18:32:07 +0900

    fix middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,7 @@
 import type { NextRequest, NextFetchEvent } from "next/server";
 import { NextResponse } from "next/server";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    if (req.ua?.isBot) {
+    if (req.ua?.isBot as boolean) {
         return new Response("Plz don't be a bot. Be human", { status: 403 });
     }
     if (!req.url.includes("/api")) {
