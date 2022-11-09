77ff834261c31c3400d9d5398b8104f5db09aa68
junyub301 <junyub0301@gmail.com>
2022-11-09 20:49:59 +0900

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
-import { NextRequest, NextFetchEvent, userAgent } from "next/server";
+import { NextRequest, NextFetchEvent } from "next/server";
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    const { isBot } = userAgent(req);
-    if (isBot) {
-        return new Response("Plz don't be a bot. Be human.", { status: 403 });
-    }
     if (!req.url.includes("/api")) {
         if (!req.url.includes("/enter") && !req.cookies.get("carrotsession")) {
             return NextResponse.redirect(new URL("/enter", req.url));
